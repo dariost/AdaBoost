69ff654b9f018c76d22fd2b7b5467fd8b7da9414
Dario Ostuni <dario.ostuni@gmail.com>
2019-08-12 17:27:34 +0200

    Print json

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,27 @@
 #include "Dataset.hpp"
 #include "DatasetView.hpp"
 #include "OneVsAll.hpp"
+#include <csignal>
+#include <cstdlib>
 #include <iomanip>
 #include <iostream>
 #include <memory>
 
 using namespace std;
 
-int main() {
+bool is_quitting = false;
+
+void sigint(int sig) {
+    (void)sig;
+    is_quitting = true;
+}
+
+int main(int argc, char* argv[]) {
+    signal(SIGINT, sigint);
+    size_t k_fold = 20;
+    if(argc > 1) {
+        k_fold = atol(argv[1]);
+    }
     size_t n_samples, n_features, n_labels;
     cin >> n_samples >> n_features >> n_labels;
     shared_ptr<Dataset> dataset = make_shared<Dataset>(n_samples, n_features, n_labels);
@@ -29,17 +43,32 @@ int main() {
         dataset->add_sample(label, point);
     }
     dataset->finalize();
-    const size_t k_fold = 20;
+    const size_t prec = 15;
     CrossValidation<OneVsAll<AdaBoost>> ab(dataset, k_fold);
     CrossValidation<OneVsAll<Bagging>> bg(dataset, k_fold);
-    for(size_t i = 0;; i++) {
+    cout << "[" << endl;
+    bool to_quit = false;
+    for(size_t i = 0; !to_quit; i++) {
+        cout << "  {" << endl;
         ab.next_epoch();
+        cout << "    \"AdaBoost\": {" << endl;
+        cout << "      \"TrainingError\": " << setprecision(prec) << ab.training_error() << "," << endl;
+        cout << "      \"TestError\": " << setprecision(prec) << ab.test_error() << endl;
+        cout << "    }," << endl;
         bg.next_epoch();
-        cout << "[AdaBoost] T = " << ab.get_epoch() << " -> tr. e. = " << setprecision(7) << ab.training_error();
-        cout << " - te. e. = " << setprecision(7) << ab.test_error() << endl;
-        cout << "[Bagging]  T = " << bg.get_epoch() << " -> tr. e. = " << setprecision(7) << bg.training_error();
-        cout << " - te. e. = " << setprecision(7) << bg.test_error() << endl;
-        cout << endl;
+        cout << "    \"Bagging\": {" << endl;
+        cout << "      \"TrainingError\": " << setprecision(prec) << bg.training_error() << "," << endl;
+        cout << "      \"TestError\": " << setprecision(prec) << bg.test_error() << endl;
+        cout << "    }" << endl;
+        cout << "  }";
+        if(is_quitting) {
+            cout << endl;
+            to_quit = true;
+        } else {
+            cout << "," << endl;
+        }
+        cerr << "T = " << (i + 1) << endl;
     }
+    cout << "]" << endl;
     return 0;
 }
